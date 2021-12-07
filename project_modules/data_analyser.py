6a9464d28bd2985f95acbcec8d3ae054230bc98d
teemvil <tee.viljanen@gmail.com>
2021-12-07 19:17:59 +0200

    modified data_analyser.py

--- project_modules/data_analyser.py
+++ project_modules/data_analyser.py
@@ -1,2 +1,46 @@
 # !/usr/bin/python
 # coding=utf-8
+
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the dataset
+data_X = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
+data_y = np.array([5, 20, 14, 32, 22, 38])
+
+# Use only one feature
+# data_X = data_X[:, np.newaxis, 2]
+
+# Split the data into training/testing sets
+data_X_train = data_X[:-2]
+data_X_test = data_X[-2:]
+
+# Split the targets into training/testing sets
+data_y_train = data_y[:-2]
+data_y_test = data_y[-2:]
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regr.fit(data_X_train, data_y_train)
+
+# Make predictions using the testing set
+data_y_pred = regr.predict(data_X_test)
+
+# The coefficients
+print("Coefficients: \n", regr.coef_)
+# The mean squared error
+print("Mean squared error: %.2f" % mean_squared_error(data_y_test, data_y_pred))
+# The coefficient of determination: 1 is perfect prediction
+print("Coefficient of determination: %.2f" % r2_score(data_y_test, data_y_pred))
+
+# Plot outputs
+#plt.scatter(data_X_test, data_y_test, color="black")
+#plt.plot(data_X_test, data_y_pred, color="blue", linewidth=3)
+
+#plt.xticks(())
+#plt.yticks(())
+
+#plt.show()
