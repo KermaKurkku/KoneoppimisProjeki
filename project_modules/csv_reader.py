d2ec395f149d13518e3c3c2c2e8b25202c54a743
jerejs <jere.salmensaari@metropolia.fi>
2021-12-08 11:17:15 +0200

    Added csv_reader

--- project_modules/csv_reader.py
+++ project_modules/csv_reader.py
@@ -1,3 +1,23 @@
-# !/usr/bin/python
-# coding=utf-8
+import pathlib
+import pandas as pd
 
+'''
+Reads a csv file using the pandas library
+Return:
+	- Pandas Dataframe object
+'''
+def read(filename: str):
+	data = pd.read_csv(str(pathlib.Path().resolve())+
+		'/Data/od-trips-2020/'+filename, index_col=0)
+	return data
+
+'''
+Reads a csv file using the pandas library and filters it according to the given station number
+
+Return
+	- Pandas Dataframe object
+'''
+def read_and_filter(filename: str, filter: int):
+	data = read(filename)
+	filter_data = data[data['Departure station id']==filter]
+	return filter_data
