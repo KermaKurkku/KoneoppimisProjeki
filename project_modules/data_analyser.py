ef88498de839aed0c71b51a95a77c9cc6464a44a
jerejs <jere.salmensaari@metropolia.fi>
2021-12-14 12:52:18 +0200

    updated data_analyzer

--- project_modules/data_analyser.py
+++ project_modules/data_analyser.py
@@ -4,77 +4,58 @@
 import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import cross_val_score
 import project_modules.csv_reader as csv_reader
 import project_modules.utils as utils
 
-# Load the dataset
-data_X = np.array([5, 15, 25, 35, 45, 55, 66, 67, 77]).reshape((-1, 1))
-data_y = np.array([5, 20, 14, 32, 22, 18, 36, 28, 14])
+regr = linear_model.LinearRegression()
+global data_X
+global data_y
+global data_X_train
+global data_X_test
+global data_y_train
+global data_y_test
 
+global data_y_pred
 
 def set_data(data):
     global data_X
-    data_X = np.array(list(data.keys())).reshape((-1, 1))
     global data_y
+    data_X = np.array(list(data.keys())).reshape((-1, 1)) 
     data_y = np.array(list(data.values()))
 
 
-# Use only one feature
-# data_X = data_X[:, np.newaxis, 2]
-
-
-# Split the data into training/testing sets
-data_X_train = data_X[:-3]
-data_X_test = data_X[-3:]
-
-
-# Split the targets into training/testing sets
-data_y_train = data_y[:-3]
-data_y_test = data_y[-3:]
-
-
 def split_data():
+    global data_X
+    global data_y
     global data_X_train
-    data_X_train = data_X[:-3]
     global data_X_test
-    data_X_test = data_X[-3:]
     global data_y_train
-    data_y_train = data_y[:-3]
     global data_y_test
+    data_X_train = data_X[:-3]
+    data_X_test = data_X[-3:]
+    data_y_train = data_y[:-3]
     data_y_test = data_y[-3:]
-
-
-# Create linear regression object
-regr = linear_model.LinearRegression()
-
-
-# Train the model using the training sets
-regr.fit(data_X_train, data_y_train)
-
 
 def train():
     global regr
     regr.fit(data_X_train, data_y_train)
-
-
-# Make predictions using the testing set
-data_y_pred = regr.predict(data_X_test)
-
+    print('score',regr.score(data_X_test, data_y_test))
+    print('Cross val score', cross_val_score(regr, data_X, data_y, cv=5))
 
 def predict():
-    global regr
     global data_y_pred
     data_y_pred = regr.predict(data_X_test)
 
 
 # The intercept (B0)
-print('intercept:', regr.intercept_)
+#print('intercept:', regr.intercept_)
 # The coefficients (B1)
-print("Coefficients: \n", regr.coef_)
+#print("Coefficients: \n", regr.coef_)
 # The mean squared error
-print("Mean squared error: %.2f" % mean_squared_error(data_y_test, data_y_pred))
+#print("Mean squared error: %.2f" % mean_squared_error(data_y_test, data_y_pred))
 # The coefficient of determination: 1 is perfect prediction
-print("Coefficient of determination: %.2f" % r2_score(data_y_test, data_y_pred))
+#print("Coefficient of determination: %.2f" % r2_score(data_y_test, data_y_pred))
 
 
 def get_coef():
